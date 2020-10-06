c30a187409884b29e3492d0fe82e0c4eb3a5cd54
Menoita99 <rui.menoita@gmail.com>
2020-10-07 00:34:13 +0100

    First assigment

---
+++ app/src/main/java/com/example/paint/DrawActivity.java
@@ -0,0 +1,36 @@
+package com.example.paint;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
+import com.pes.androidmaterialcolorpickerdialog.ColorPickerCallback;
+
+public class DrawActivity extends Activity {
+
+    private int color = Color.WHITE;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.draw);
+        setBackgroundColor(color);
+    }
+
+    private void setBackgroundColor(int newColor) {
+        color = newColor;
+        View rootView = findViewById(android.R.id.content).getRootView();
+        rootView.setBackgroundColor(color);
+    }
+
+
+    public void showColorSelector(View v){
+        final ColorPicker cp = new ColorPicker(this, Color.red(color), Color.green(color), Color.blue(color));
+        cp.enableAutoClose();
+        cp.setCallback(this::setBackgroundColor);
+        cp.show();
+    }
+}
--- app/src/main/java/com/example/paint/MainActivity.java
+++ app/src/main/java/com/example/paint/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.paint;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private final static long TIME_TO_WELCOME = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(() -> {
+                Intent i = new Intent(MainActivity.this, DrawActivity.class);
+                startActivity(i);
+                MainActivity.this.finish();
+            },TIME_TO_WELCOME);
     }
-
-
-
-
 }
