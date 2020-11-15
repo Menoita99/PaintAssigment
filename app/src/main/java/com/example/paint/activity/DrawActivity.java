43305c2481458424122dadfb7e6c1ac9f1268562
Francisco Cardoso <60990445+Franciscodc98@users.noreply.github.com>
2020-11-15 20:12:24 +0000

    Sensors

--- app/src/main/java/com/example/paint/activity/DrawActivity.java
+++ app/src/main/java/com/example/paint/activity/DrawActivity.java
@@ -1,9 +1,13 @@
 package com.example.paint.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import androidx.fragment.app.FragmentActivity;
 
@@ -15,6 +19,11 @@ import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
 
 public class DrawActivity extends FragmentActivity {
 
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor, lightSensor;
+    private boolean isAccelerometerAvailable, isLightSensorAvailable;
+
+
     private CanvasFragment canvasFragment;
     private MenuFragment menuFragment;
 
@@ -25,6 +34,7 @@ public class DrawActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.draw);
+        setupSensor();
         canvasFragment = (CanvasFragment) getSupportFragmentManager().findFragmentById(R.id.canvas);
         menuFragment = (MenuFragment) getSupportFragmentManager().findFragmentById(R.id.menu);
         history.add(canvasFragment.getCanvas().getPaintColor());
@@ -82,4 +92,46 @@ public class DrawActivity extends FragmentActivity {
         Intent i = new Intent(this, MapsActivity.class);
         startActivity(i);
     }
+
+
+    private void setupSensor(){
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)!= null){
+            accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+            isAccelerometerAvailable = true;
+        }else {
+            isAccelerometerAvailable = false;
+            System.out.println("Accelerometer not available");
+        }
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)!= null){
+            lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+            isLightSensorAvailable = true;
+        }else {
+            isLightSensorAvailable = false;
+            System.out.println("Light sensor not available");
+        }
+    }
+
+    public SensorManager getSensorManager(){
+        return sensorManager;
+    }
+
+    public Sensor getLightSensor(){
+        return lightSensor;
+    }
+
+    public Sensor getAccelerometerSensor(){
+        return accelerometerSensor;
+    }
+
+    public boolean getIsAccelerometerAvailable(){
+        return isAccelerometerAvailable;
+    }
+    public boolean getIsLightSensorAvailable(){
+
+        return isLightSensorAvailable;
+    }
+
+
 }
